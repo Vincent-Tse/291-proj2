eea5d76126a4986265bd9bbed863f0eb2110713b
Shi Yan <ssyan@cs.ualberta.ca>
2016-11-22 01:25:54 -0700

    att closure added

--- main.py
+++ main.py
@@ -31,31 +31,80 @@ def main():
             clearScreen()
             BCNF(conn)
             return
+        else:
+            return
 
 
 def thirdNF(conn):
+    readInputRelation(conn)
     pass
 def BCNF(conn):
+    readInputRelation(conn)
     pass
-def attrClos():
-    c = conn.cursor()
+def attrClos(conn):
 
-    c.execute("select * from Input_FDs_R1") #HELP!!!! I can't generalize to all table
-    relation = c.fetchall()
-
-    closure = raw_input("The attribute :")
-    for dependency in relation:
-        RHS = dependency[0]
-        LHS = dependency[1]
-        if RHS in closure:
-            for char in LHS:
-                if char not in closure:
-                    closure+=char
+    closure = raw_input("attribute: ")
+    print(closure)
+    relation = readInputRelation(conn)
+    while True:
+        old = closure
+        for dependency in relation:
+            LHS = "".join(dependency[0].split(","))
+            RHS = "".join(dependency[1].split(","))
+            if all(char in closure for char in LHS) and not any(letter in closure for letter in RHS):
+                closure+=RHS
+        if old == closure:
+            break
     print(closure)
     conn.commit()
 
+
+
+
 def attrEquivalence():
     pass
+
+def readInputRelation(conn):
+    c = conn.cursor()
+    menu=[]
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    rows=c.fetchall()
+    while True:
+        print("Make selection")
+        for i in range(len(rows)):
+            for item in rows[i]:
+                item =item.encode("utf-8")
+                choice =str(i+1)+')'+item
+                print(choice)
+                menu.append(item)
+        try:
+            selection = int(raw_input("Selection: "))
+            if selection >len(rows)+1 or selection<1:
+                raise
+        except:
+            clearScreen()
+            print("Invalid Input")
+            raw_input("")
+            continue    # not an integer
+        else:
+            break
+    c.execute("SELECT * FROM "+menu[selection-1]+";")
+    rows = c.fetchall()
+
+    table =[]
+    for row in rows:
+        line=[]
+        for i in range(len(row)):
+            if type(row[i]) == unicode:
+                data = row[i].encode("utf-8")
+            else:
+                data = row[i]
+            line.append(data)
+        table.append(line)
+    conn.commit()
+    return(table)
+
+
 def clearScreen():
     os.system("clear")
 main()
